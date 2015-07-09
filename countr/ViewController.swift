6b469300b86e69d75e8118675cf256b06c302570
Peter Widmann <peterwidmann@Peters-iMac.local>
2015-07-09 19:15:29 +0200

    Initial Commit

---
+++ countr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  countr
+//
+//  Created by Peter Widmann on 09.07.15.
+//  Copyright (c) 2015 Digitales Handwerk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
